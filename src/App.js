1067432d8bf59233c916af94aa21829caa0e35c2
Nisan Dekel <nisandekel@walla.com>
2021-02-26 16:00:08 +0200

    use react bootstrap to create a table on Form List page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import FormList from "./components/FormList";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
-  state = { formList: ["sa","aas"] };
+  state = { formList: [{id:1,name:"form1",numOfSubmissions: 0,submitPageLink:"link1" },{id:2,name:"form2",numOfSubmissions: 0,submitPageLink:"link2" }] };
 
   render() {
     return (
--- src/components/FormList.jsx
+++ src/components/FormList.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React from "react";
+import { Button, Table } from "react-bootstrap";
 
 function FormList(props) {
-    
-    const forms = props.forms.map(form => <div>{form}</div>);
+  const forms = props.forms.map((form) => {
+    return (<tr id={"row_" + form.id} key={form.id}>
+        <td>{form.id}</td>
+        <td>{form.name}</td>
+        <td>{form.numOfSubmissions}</td>
+        <td>{form.submitPageLink}</td>
+    </tr>);
+  });
 
-    return (
-      <div className="FormList">
-          {forms}
-      </div>
-    );
+  return (
+    <div className="FormList">
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>Form Id</th>
+            <th>Form Name</th>
+            <th>#Submissions</th>
+            <th>Submit Page</th>
+          </tr>
+        </thead>
+        <tbody>
+        {forms}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 
 export default FormList;
